12b230f449da75c5b618f9f932d12fbf116b5bd5
Bharanidharan <bharanidharanbharath@gmail.com>
2023-09-01 18:33:11 +0530

    first commit

---
+++ src/components/growing tree/GrowingTree.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './GrowingTree.css'
+
+function GrowingTree() {
+
+    const hello = {
+       color : 'red'
+    }
+  return (
+    <>
+    <div class = "hello">GrowingTree</div>
+    <div style = {hello}>Nikola Tesla</div>
+    </>
+  )
+}
+
+export default GrowingTree
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import GrowingTree from '../growing tree/GrowingTree'
+
+function Home() {
+  return (
+    <>
+    <GrowingTree/>
+    </>
+    
+  )
+}
+
+export default Home
